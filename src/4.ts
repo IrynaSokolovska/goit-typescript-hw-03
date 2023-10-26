c8227332e2222480e3a7e55f77cfc1abd3ba1ad4
Iryna Sokolovska <sokirina777@gmail.com>
2023-10-26 21:46:01 +0300

    first

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,62 @@
-const key = new Key();
+class Key {
+    private signature: number;
 
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+};
+
+
+class Person{
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+};
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+        this.door = false;
+        this.tenants = [];
+    }
+
+    comeIn(person: Person) {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log("Людина заходить.");
+        } else {
+             console.log("Двері зачинені.");
+        }
+    }
+    abstract openDoor(key: Key): void;
+};
+
+class MyHouse extends House{
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("Двері відчинені.");
+        } else {
+            console.log("Ключ не підійшов до двері.");
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
